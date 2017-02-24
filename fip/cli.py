148ed8c8293664915ff014557e6bc04c5a040020
Matthieu Berjon <matthieu@berjon.net>
2017-02-24 22:36:12 +0100

    Add of a display function and call of the library
    I added a call to the library as well as a display function for the
    command-line client.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- fip/cli.py
+++ fip/cli.py
@@ -3,6 +3,7 @@
 from . import __version__
 import click
 import sys
+from . import fip
 
 
 def version_msg():
@@ -13,33 +14,50 @@ def version_msg():
     return message.format(python_version)
 
 
+def display(data):
+    title = "Title: {0}".format(data['title'])
+    artist = "Artist: {0}".format(data['authors'])
+    album = "Album: {0} ({1})".format(
+            data['titreAlbum'],
+            data['anneeEditionMusique'])
+    label = "Label: {0}".format(data['label'])
+    print(title)
+    print(artist)
+    print(album)
+    print(label)
+
+
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option(
-        '-c',
-        '--current',
-        is_flag=True,
-        help='Display the current song information')
+    '-c',
+    '--current-song',
+    is_flag=True,
+    help='Display the current song information')
 @click.option(
-        '-n',
-        '--next',
-        is_flag=True,
-        help='Display the next song information')
+    '-n',
+    '--next-song',
+    is_flag=True,
+    help='Display the next song information')
 @click.option(
-        '-s',
-        '--store',
-        is_flag=True,
-        help='Save the current song information')
+    '-s',
+    '--save',
+    is_flag=True,
+    help='Save the current song information')
 @click.version_option(
-        __version__,
-        '-V',
-        '--version',
-        message=version_msg(),
-        help='Ouput the version of this application')
-def main(current, next, save):
-    """Console script for fip_music"""
-    click.echo("Replace this message by putting your code into "
-               "fip.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    __version__,
+    '-V',
+    '--version',
+    message=version_msg(),
+    help='Ouput the version of this application')
+def main(current_song, next_song, save):
+    if current_song:
+        data = fip.Fip()
+        data.request_data()
+        display(data.data)
+    elif next_song:
+        data = fip.Fip()
+        data.request_data(offset=1)
+        display(data.data)
 
 
 if __name__ == "__main__":
