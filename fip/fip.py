c863a870bc17759e6340847fb2cb245f9ab48f26
Matthieu Berjon <matthieu@berjon.net>
2017-02-24 22:23:26 +0100

    Adding of the request_data method
    I added a method that request the data from the server, extract what it
    needs according to the offset and store it in the object data structure.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- fip/fip.py
+++ fip/fip.py
@@ -4,10 +4,16 @@ import requests
 class Fip():
 
     def __init__(self):
-        pass
+        self.url = 'http://www.fipradio.fr/livemeta/7'
+        self.data = {}
+        self.uid = []
 
-    def request_data(self):
-        pass
+    def request_data(self, offset=0):
+        data = requests.get(self.url).json()
+        level = data['levels'][0]
+        position = level['position'] + offset
+        self.uid = level['items'][position]
+        self.data = data['steps'][self.uid]
 
     def save_data(self):
         pass
