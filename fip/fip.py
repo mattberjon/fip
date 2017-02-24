971e71ab0f3ffdb6bf5e9328fc102e9bc1baa0f7
Matthieu Berjon <matthieu@berjon.net>
2017-02-24 22:30:31 +0100

    Update of the docstrings
    In order to have something a bit better, I added the docstrings
    describing the current code.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- fip/fip.py
+++ fip/fip.py
@@ -1,14 +1,45 @@
+""" Fip module
+
+This module manages the Fip class in order to request the data and save them.
+
+Todo:
+    - Improve docstrings
+    - Log eventually events
+    - Add an authentification system in another file or here
+
+"""
+
 import requests
 
 
 class Fip():
+    """ Fip class
+
+    Instanciate an object that will request data from Fip server
+
+    """
 
     def __init__(self):
+        """ Constructor of the Fip class
+
+        Initialize the instance
+
+        """
         self.url = 'http://www.fipradio.fr/livemeta/7'
         self.data = {}
         self.uid = []
 
     def request_data(self, offset=0):
+        """ Request the data to the server
+
+        Send a request to the server to grab a specific JSON file containing
+        the information related to lastest songs played on the radio.
+
+        Args:
+            offset (int):   Offset to grab the next song information (but could
+                            be the previous too).
+
+        """
         data = requests.get(self.url).json()
         level = data['levels'][0]
         position = level['position'] + offset
